09e500fdab17ca1ae0d3d1b7d655b0826817ef2e
daesanTheRiceologist <daesan98@gmail.com>
2023-03-04 10:34:24 -0800

    navbar component created

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+
+
+export default function NavBar(){
+    return(
+        <nav className='flex justify-center'>
+            <img className='w-1/4' src="travelgenieLogo.svg" alt="travel genie Logo"/>
+        </nav>
+    )
+}
