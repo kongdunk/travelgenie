18614b2fc551210dffc94d50e26cb25ab726f6c1
daesanTheRiceologist <daesan98@gmail.com>
2023-03-05 21:44:49 -0800

    input component created

---
+++ components/searchInput.js
@@ -0,0 +1,5 @@
+export default function SearchInput(){
+    return(
+        <input type="text" placeholder='Where do you want to travel?' className='border-4 w-1/2 border-indigo-500 rounded-lg h-10 pl-10 m-5'/>
+    )
+}
