529bd1a41b757b4f47b98543519c57577940d6ad
daesanTheRiceologist <daesan98@gmail.com>
2023-03-06 17:55:24 -0800

    cards component created

---
+++ components/cards.js
@@ -0,0 +1,28 @@
+export default function Card(){
+    return (
+        <>
+        <div className='w-5/6 flex justify-center flex-col rounded-lg p-5 m-8'>
+        <div className="flex items-center justify-between">
+            <h2 className='text-2xl font-semibold mb-3'>
+                3 Days in tokyo
+            </h2>
+            <p className="mb-3 font-bold">
+                @daesan
+            </p>
+        </div>
+        <p>
+        Start the day by visiting Sensoji Temple, one of the most popular and oldest temples in Tokyo. You can also explore the surrounding Asakusa district and grab some traditional Japanese street food.
+        </p>
+        <br/>
+        <p> 
+        Start the day by visiting Sensoji Temple, one of the most popular and oldest temples in Tokyo. You can also explore the surrounding Asakusa district and grab some traditional Japanese street food.
+        </p>
+        <br/>
+        <p>
+        Start the day by visiting Sensoji Temple, one of the most popular and oldest temples in Tokyo. You can also explore the surrounding Asakusa district and grab some traditional Japanese street food.
+        </p>
+    </div>
+    <div className=" border-b-2 w-full "/>
+    </>
+    )
+}
