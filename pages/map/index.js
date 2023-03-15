4a24850306389174078ebc27b4fb3ad1ca571013
daesanTheRiceologist <daesan98@gmail.com>
2023-03-15 01:21:16 -0700

    map continent pages added

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,11 +1,15 @@
 import NavBar from '@/components/navbar';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
+
 const ReactDOM = require('react-dom');
 const WorldMap = require('react-world-map');
 
 export default function Account(){
     const [selected, onSelect] = useState(null);
+    const router = useRouter()
 
+    //for displaying selected continent
     const continents = {
         na: "North America",
         sa: "South America",
@@ -13,6 +17,16 @@ export default function Account(){
         eu: "Europe",
         as: "Asia",
         oc: "Oceania",
+    }
+
+    //for routing to selected continent page
+    const lowerCasedContinents = {
+        na: "northamerica",
+        sa: "southamerica",
+        af: "africa",
+        eu: "europe",
+        as: "asia",
+        oc: "oceania",
     }
     
     return (
@@ -22,6 +36,11 @@ export default function Account(){
         <WorldMap selected={ selected } onSelect={ onSelect }/>
     </div>
     <div className='w-full flex justify-center font-semibold'> selected region: {continents[selected]}</div>
+    <div className='w-full flex justify-center'>
+        <button onClick={() => router.push("map/" + lowerCasedContinents[selected])} className= 'bg-slate-500 m-2 p-2 rounded-xl text-white font-bold'>
+            select region
+        </button>
+    </div>
     </>
 
     )
